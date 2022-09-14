472f9419c2e9ef9e1d9629e9e28c6cd200f1b3eb
IvanIvanson <ghalut1977@gmail.com>
2022-09-14 18:46:27 +0300

    добавил reset и clear

--- index.js
+++ index.js
@@ -1,28 +1,75 @@
- const btnStart = document.querySelector("#start");
-        const btnStop = document.querySelector("#stop");
-        let a = 1;
-        let timer;
-        let b = document.getElementById("bd")
-        let p = document.getElementById("timer");
-
-
-        function myTimer() {
+ window.onload=function(){
+    const btnStart = document.querySelector("#start");
+    const btnStop = document.querySelector("#stop");
+    const btnReset = document.querySelector("#reset");
+    const blockSave = document.querySelector("#block-save");
+    const btnClear = document.querySelector("#clear");
+   
+    //----------   
+    let a = 1;
+    let timer;
+    let b = document.getElementById("bd")
+    let p = document.getElementById("timer");   
+    //----------
+    
+      function createElement(text){
+      let save = document.createElement("div");
+      save.classList.add("infodiv")
+      save.textContent=text;
+          blockSave.appendChild(save);
+      }
+        
+            function myTimer(){
             p.classList.add("blink")
-            p.innerHTML = `${a} s`;
-            let min = Math.floor(a / 60)
-            a++;
-            if (a >= 60) {
-                p.innerHTML = `${min}min:${a - (min * 60)}s`;
-                p.style.fontSize = 20 + 'px';
-            }
-            timer = setTimeout(myTimer, 1000);
-
-            btnStop.addEventListener("click", function () {
-                clearTimeout(timer);
-                p.classList.remove("blink")
-            })
-        }
-
-        // --------
-
-        btnStart.addEventListener("click", myTimer);
+            p.innerHTML=`${a} s`;
+            let min = Math.floor(a/60)
+      a++;
+      if(a>=60){
+          p.innerText=`${min}min:${a-(min*60)}s`;
+          p.style.fontSize=20+'px';
+      }
+      
+      }
+        //---------------
+    
+     btnStop.addEventListener("click", function(){
+     let inp = document.querySelector("#inp");
+     let info = inp.value;
+     inp.value = '';
+         clearInterval(timer);
+         p.classList.remove("blink");                
+         createElement(`${info} - ${a-1}s`)
+                                       
+     });
+    
+     //-----------
+    
+     btnReset.addEventListener("click", function(){
+     let inp = document.querySelector("#inp");
+     inp.value = '';
+    
+         clearInterval(timer);
+         a = 0;
+         p.classList.remove("blink");
+        
+         p.innerText = a + ' s';
+        
+     })
+           
+       // -------
+       
+    btnStart.addEventListener("click", function(){
+   
+       timer = setInterval(myTimer, 1000);
+    });
+   
+    //---------
+    btnClear.addEventListener("click", function(){
+    let allOut = document.querySelectorAll(".infodiv");
+    allOut.forEach((item) => {
+        item.remove()
+    })
+       
+    })
+   
+}
